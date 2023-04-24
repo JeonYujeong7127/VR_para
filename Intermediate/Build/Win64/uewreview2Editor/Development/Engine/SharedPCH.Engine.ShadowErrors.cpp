1b99bc5bf63530b7751cff4f87ec52e1e6243dec
진욱 송 <wlsdnr6837@gmail.com>
2023-04-24 13:45:34 +0900

    Add project files.

---
+++ Intermediate/Build/Win64/uewreview2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32217
+#include "C:/Users/wlsdn/UnrealFile/Unreal Projects/uewreview2/Intermediate/Build/Win64/uewreview2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/uewreview2/MyActorddddd.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActorddddd.h"
+
+// Sets default values
+AMyActorddddd::AMyActorddddd()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActorddddd::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActorddddd::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
